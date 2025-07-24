ff481a1eb8efca24232d6838356b28fdfd59b9b3
Gomaa <mohamedmanno2003@gmail.com>
2025-07-24 15:47:49 +0300

    file serve

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import express from 'express'
 const app = express();
 
 app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.send('<h1 style="color:blue;text-align:center;">Hello, World!</h1>');
+    res.sendFile('./public/main.html', { root: process.cwd() });
 });
 
 const PORT = 3000;
